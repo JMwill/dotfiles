a8ab158eb137dec49f407a132f36d08a91120be6
JMwill <787505998@qq.com>
2018-07-03 23:32:11 +0800

    添加安装前配置确认

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 DOTFILES_PATH=~/.dotfiles
 
-echo "Need you to enter your password to install..."
-# Keep-alive update existing sudo time stamp until script has finished
-sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+read -p "Did you setup the valid ssh config? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Need you to enter your password to install..."
+  # Keep-alive update existing sudo time stamp until script has finished
+  sudo -v
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# System Specific
-if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
-  . "$DOTFILES_PATH"/osx/install.sh
-else
-  . "$DOTFILES_PATH"/ubuntu/install.sh
-fi
+  # System Specific
+  if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
+    . "$DOTFILES_PATH"/osx/install.sh
+  else
+    . "$DOTFILES_PATH"/ubuntu/install.sh
+  fi
+fi;
