4f70412dc14a133ab9c7b63f9efac26c328f21dc
JMwill <787505998@qq.com>
2018-09-27 09:54:57 +0800

    ubuntu 安装时添加判断当前环境为服务器版本或桌面版的逻辑

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,16 +4,22 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo apt-get -y install build-essential curl file git unzip
 
-# vscode preinstall
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+# Desktop install
+if [[ "$XDG_CURRENT_DESKTOP" ]]; then
+  # vscode preinstall
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt-get -y update
+  sudo apt-get -y update
+  desktopapps=(
+    xsel
+    code
+  )
+  sudo apt-get install -y ${desktopapps[@]}
+fi
 
 cliapps=(
-  xsel
-  code
   zsh
 )
 
