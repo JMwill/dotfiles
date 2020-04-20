4e9ec9733e0bd68c072ba66aa13825d3b383ddd6
JMwill <willsurpriseyou@foxmail.com>
2020-04-20 22:52:12 +0800

    add org capture templates for hugo

--- .custom.el
+++ .custom.el
@@ -26,12 +26,33 @@
 
 
 ;; Set up org capture template
+;; Populates only the EXPORT_FILE_NAME property in the inserted headline.
+(with-eval-after-load 'org-capture
+  (defun org-hugo-new-subtree-post-capture-template ()
+    "Returns `org-capture' template string for new Hugo post. See `org-capture-templates' for more information."
+    (let* ((title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
+	   (fname (org-hugo-slug title)))
+      (mapconcat #'identity
+		 `(
+		   ,(concat "* TODO " title)
+		   ":PROPERTIES:"
+		   ,(concat ":EXPORT_FILE_NAME: " (format-time-string "%Y-%m-%d-") fname)
+		   ":END:"
+		   "%?\n")          ;Place the cursor here finally
+		 "\n"))))
+
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
                                (file+headline "~/gtd/inbox.org" "Tasks")
                                "* TODO %i%?")
                               ("T" "Tickler" entry
                                (file+headline "~/gtd/tickler.org" "Tickler")
-                               "* %i%? \n %U")))
+                               "* %i%? \n %U")
+                              ("h" "Hugo post" entry
+                                ;; It is assumed that below file is present in `org-directory'
+                                ;; and that it has a "Blog Ideas" heading. It can even be a
+                                ;; symlink pointing to the actual location of all-posts.org!
+                               (file+olp "~/Personal/blog/content-org/2020.org" "Posts")
+                               (function org-hugo-new-subtree-post-capture-template))))
 
 ;; Set up org refile targets
 (setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
